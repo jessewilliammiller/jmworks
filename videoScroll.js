66415a10f11a843d3fabce9a2961d0ae7622ed40
Alex Brian Naughton <alexbriannaughton@gmail.com>
2023-04-05 13:11:15 -0500

    pause video on next/prev click

--- videoScroll.js
+++ videoScroll.js
@@ -1,19 +1,19 @@
-const scrollContainer = document.getElementById('vid-container');
-const scrollLeftButton = document.getElementById('scroll-left-button');
-const scrollRightButton = document.getElementById('scroll-right-button');
+// const scrollContainer = document.getElementById('vid-container');
+// const scrollLeftButton = document.getElementById('scroll-left-button');
+// const scrollRightButton = document.getElementById('scroll-right-button');
 
-scrollLeftButton.addEventListener('click', () => {
-    if (scrollContainer.scrollLeft - (scrollContainer.scrollWidth / 13) <= 0) {
-        scrollContainer.scrollLeft = scrollContainer.scrollWidth
-    }
-    else scrollContainer.scrollLeft -= scrollContainer.scrollWidth / 13;
-});
+// scrollLeftButton.addEventListener('click', () => {
+//     if (scrollContainer.scrollLeft - (scrollContainer.scrollWidth / 13) <= 0) {
+//         scrollContainer.scrollLeft = scrollContainer.scrollWidth
+//     }
+//     else scrollContainer.scrollLeft -= scrollContainer.scrollWidth / 13;
+// });
 
-scrollRightButton.onclick = () => {
-    if (scrollContainer.scrollLeft + (scrollContainer.scrollWidth / 13) >= scrollContainer.scrollWidth) {
-        scrollContainer.scrollLeft = 0
-    }
-    else {
-        scrollContainer.scrollLeft += scrollContainer.scrollWidth / 13;
-    }
-};
+// scrollRightButton.onclick = () => {
+//     if (scrollContainer.scrollLeft + (scrollContainer.scrollWidth / 13) >= scrollContainer.scrollWidth) {
+//         scrollContainer.scrollLeft = 0
+//     }
+//     else {
+//         scrollContainer.scrollLeft += scrollContainer.scrollWidth / 13;
+//     }
+// };
