177f5ac318ea47faae6c4b7654bc1c07f64539f4
Alex Brian Naughton <alexbriannaughton@gmail.com>
2023-05-03 16:42:13 -0700

    live.js

--- js/live.js
+++ js/live.js
@@ -1,45 +1,132 @@
-function Show(upcoming, date, venue, band, misc, link) {
+function Show(upcoming, line1, line2, link) {
     this.upcoming = upcoming;
-    this.date = date;
-    this.venue = venue;
-    this.band = band;
-    this.misc = misc;
+    this.line1 = line1;
+    this.line2 = line2
     this.link = link;
-
 }
 
 const allShows = [
+    // true indicates upcoming show
+    // false indicates past show
+    // add a space within the quotes by adding <br/>
     new Show(
         true,
-        "May 6, 2023",
-        "Gallery 1412",
-        "JM Fodder",
-        "w/ Patrick Toney"
+        "May 6: JM Fodder @ Gallery 1412 w/ Patrick Toney",
+        "Seattle, WA"
+    ),
+    new Show(
+        true,
+        "May 13 JM Fodder @ Bad Bar",
+        "Seattle, WA"
+    ),
+    new Show(
+        true,
+        "May 21st -w /Joe Waine @ Sunset Tavern",
+        "Seattle, WA"
+    ),
+    new Show(
+        true,
+        "June 4th w/ Joe Waine @ Royal Room",
+        "Seattle, WA"
+    ),
+    new Show(
+        true,
+        "June 24th, 2023 JM Fodder @ Woodland Theatre",
+        "Seattle, WA"
     ),
     new Show(
         false,
-        "April 27, 2023",
-        "Fremont Abbey",
-        "Joe Waine",
-        "w/ Crushing"
+        "April 27 w/ Joe Waine & Crushing @ Fremont Abbey",
+        "Seattle, WA"
+    ),
+    new Show(
+        false,
+        "March 31: Jonny G and the Music Factory @ Swan Dive",
+        "Portland, OR"
+    ),
+    new Show(
+        false,
+        "April 1: Jonny G and the Music Factory @ Souwester Arts",
+        "Seaview, OR"
+    ),
+    new Show(
+        false,
+        "April 2: Jonny G and the Music Factory @ Ft. George Brewery",
+        "Astoria, OR"
+    ),
+    new Show(
+        false,
+        "April 8 - JM Fodder & Rachel Ewen @ Woodland Studios",
+        "Playing guitar in Rachel Ewen's band (our first show!) and doing my improv computer music thing as JM Fodder<br/>Seattle, WA"
+    ),
+    new Show(
+        false,
+        "March 29 w/ Jonny G @ Bad Bar (Tour Kickoff!)",
+        "Seattle, WA"
+    ),
+    new Show(
+        false,
+        "March 30: Jonny G and the Music Factory @ House Show",
+        "Olympia, WA"
+    ),
+    new Show(
+        false,
+        "March 24: Fodder/Peg (duo) @ The Vera Project",
+        "Seattle, WA"
+    ),
+    new Show(
+        false,
+        "March 21 w/ Peg @ Sunset Tavern",
+        "Seattle, WA"
+    ),
+    new Show(
+        false,
+        "March 11 - JM Fodder (record release) and Joe Waine band @ Bad Bar",
+        "My big record release, don't miss this one!<br/>Seattle, WA"
+    ),
+    new Show(
+        false,
+        "March 1 w/ Joe Waine @ Here After (The Crocodile)",
+        "Seattle, WA"
+    ),
+    new Show(
+        false,
+        "Peg Album Release show! February 19 @ Woodland Studios",
+        "Guitar w/ Peg, also on the bill is Ronan Delisle. very excited for this one:)<br/>Seattle, WA"
+    ),
+    new Show(
+        false,
+        "January 27 @ Conor Byrne Pub",
+        "Guitar w/ Joe Waine Band<br>Seattle, WA"
+    ),
+    new Show(
+        false,
+        "NYE @ Blue Moon Tavern",
+        "I’ll be playing guitar with the Joe Waine, also performing is Scott Yoder, Reverse Death (Record Release) and 129,600.<br/>Tickets at the door, $8, 8pm-2am<br/>Seattle, WA"
+    ),
+    new Show(
+        false,
+        "December 17th @ House Party (contact for address, details)",
+        "I’ll be doing a computer based, electronic set of Reggaeton music as JM Fodder, both original and DJ selections."
+    ),
+    new Show(
+        false,
+        "December 11th @ Sunset Tavern",
+        "I'll be playing guitar with the Joe Waine Band<br/>Seattle, WA"
     )
 ]
-
-function renderLink(link) {
-    if (link) {
-        return `<a href="${link}" target="_blank">tix</a>`
-    }
-}
 
 const upcomingDiv = document.getElementById('upcoming-div')
 
 const upcomingHTML = allShows.filter((show) => show.upcoming)
     .map(upcomingShow => `
         <div class="oneShow">
-            <p>${upcomingShow.date}: ${upcomingShow.band} @ ${upcomingShow.venue} ${upcomingShow.misc}</p>
-            ${renderLink(upcomingShow.link)}
+            <p>${upcomingShow.line1}</p>
+            <p>${upcomingShow.line2}</p>
+            ${upcomingShow.link
+            ? `<a href="${upcomingShow.link}" target="_blank">tix</a>`
+            : ""}
         </div>
-
         <hr class="lilLine"></hr>
     `).join("");
 
@@ -48,12 +135,12 @@ upcomingDiv.innerHTML = upcomingHTML
 const pastDiv = document.getElementById('past-div')
 
 const pastHTML = allShows.filter((show) => !show.upcoming)
-.map(pastShow => `
+    .map(pastShow => `
         <div class="oneShow">
-            <p>${pastShow.date}: ${pastShow.band} @ ${pastShow.venue} ${pastShow.misc}</p>
+            <p>${pastShow.line1}</p>
+            <p>${pastShow.line2}</p>
         </div>
-
         <hr class="lilLine"></hr>
-    `).join("");
+`).join("");
 
 pastDiv.innerHTML = pastHTML
